0e0baaec13cd9c3296ce088d9ffc0ff6cd3bbc3b
Nandakumar T <nandakumart07@gmail.com>
2023-05-15 10:22:36 +0530

    dayily array leetcode

---
+++ arry/1491-leet.js
@@ -0,0 +1,24 @@
+var average = function(salary) {
+    let max=salary[0]
+    let min=salary[0]
+    for(let i=1;i<salary.length;i++){
+        if(salary[i]>max){
+            max=salary[i]
+        }
+        if(salary[i]<min){
+            min=salary[i]
+        }
+    }
+    let sum=0
+    for(let i=0;i<salary.length;i++){
+        if(max!=salary[i] && min !=salary[i]){
+            sum+=salary[i]
+        }
+    }
+    let avg=sum/(salary.length-2)
+    return avg
+};
+
+let salary=[4000,3000,1000,2000]
+let res=average(salary)
+console.log(res);
