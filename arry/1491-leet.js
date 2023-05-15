c7512947821514bd4e9af65a7895b0bd7b74a49f
Nandakumar T <nandakumart07@gmail.com>
2023-05-15 11:48:03 +0530

    optimized solution

--- arry/1491-leet.js
+++ arry/1491-leet.js
@@ -1,22 +1,39 @@
+// var average = function(salary) {
+//     let max=salary[0]
+//     let min=salary[0]
+//     for(let i=1;i<salary.length;i++){
+//         if(salary[i]>max){
+//             max=salary[i]
+//         }
+//         if(salary[i]<min){
+//             min=salary[i]
+//         }
+//     }
+//     let sum=0
+//     for(let i=0;i<salary.length;i++){
+//         if(max!=salary[i] && min !=salary[i]){
+//             sum+=salary[i]
+//         }
+//     }
+//     let avg=sum/(salary.length-2)
+//     return avg
+// };
+
+// optimized solution with O(n) time
+
 var average = function(salary) {
-    let max=salary[0]
-    let min=salary[0]
-    for(let i=1;i<salary.length;i++){
-        if(salary[i]>max){
-            max=salary[i]
-        }
-        if(salary[i]<min){
-            min=salary[i]
-        }
+    let val=0;
+    let max= -Infinity;
+    let min= Infinity;
+    for(let el of salary){
+        val+=el;
+    max<el?max=el:max=max;
+    min>el?min=el:min=min
     }
-    let sum=0
-    for(let i=0;i<salary.length;i++){
-        if(max!=salary[i] && min !=salary[i]){
-            sum+=salary[i]
-        }
-    }
-    let avg=sum/(salary.length-2)
-    return avg
+
+ let ans= val-(max+min);
+ ans= ans/(salary.length-2);
+ return ans
 };
 
 let salary=[4000,3000,1000,2000]
