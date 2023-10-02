4f695b5129391399742c6008bd6c1025f7656ba5
Nandakumar T <nandakumart07@gmail.com>
2023-10-02 11:48:42 +0530

    react-hooks-started

---
+++ week-18-react/hooks-yt/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ClassCounter from './Components/ClassCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassCounter />
+    </div>
+  );
+}
+
+export default App;
