b21d3335438fd494c66c94aba1d0c7d13e4b408f
Nandakumar T <nandakumart07@gmail.com>
2024-01-25 21:05:11 +0530

    progressBar

---
+++ learnReact/downloadprogress/src/App.js
@@ -0,0 +1,12 @@
+import ProgressBar from "./ProgressBar";
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ProgressBar />
+    </div>
+  );
+}
+
+export default App;
